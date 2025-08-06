c17ce9fd14ee1b7e8b545211a336a8bec416d65c
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-06 11:19:08 +0900

    .

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-
+const characterRouter = require('./routes/character')
 
 app.use(express.json())
-
+app.use('/char',characterRouter)
 
 
 
